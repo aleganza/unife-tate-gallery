72799d7d8942f19af37d96a7062b919a87c78bed
Simone Acuti <acutisimone@gmail.com>
2024-06-28 13:45:50 +0200

    feat: add navbar

---
+++ src/php/components/Navbar.php
@@ -0,0 +1,11 @@
+<header>
+  <nav>
+    <p>LOGO</p>
+  </nav>
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="opere.php">Opere</a>
+    <a href="ricerca_opere.php">Ricerca Opere</a>
+    <a href="statistiche.php">Statistiche</a>
+  </nav>
+</header>
