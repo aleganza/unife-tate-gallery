92c454bf271f63419deeed28decd312d9502e448
aleganza <ganzarolli.alessio@gmail.com>
2024-06-28 20:54:51 +0200

    feat: pages

--- src/php/components/Navbar.php
+++ src/php/components/Navbar.php
@@ -3,9 +3,14 @@
     <p>LOGO</p>
   </nav>
   <nav>
-    <a href="index.php">Home</a>
-    <a href="opere.php">Opere</a>
-    <a href="ricerca_opere.php">Ricerca Opere</a>
-    <a href="statistiche.php">Statistiche</a>
+    <?php
+    $current_page = basename($_SERVER['PHP_SELF']);
+    ?>
+    <a href="index.php" class="<?= ($current_page == 'index.php') ? 'active' : '' ?>">Home</a>
+    <a href="artisti.php" class="<?= ($current_page == 'artisti.php') ? 'active' : '' ?>">Artisti</a>
+    <a href="correlazioni.php" class="<?= ($current_page == 'correlazioni.php') ? 'active' : '' ?>">Correlazioni</a>
+    <a href="opere.php" class="<?= ($current_page == 'opere.php') ? 'active' : '' ?>">Opere</a>
+    <a href="statistiche.php" class="<?= ($current_page == 'statistiche.php') ? 'active' : '' ?>">Statistiche</a>
   </nav>
 </header>
+<hr />
--- src/php/views/index.php
+++ src/php/views/index.php
@@ -1,15 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="../../css/index.css">
-  <title>Tate gallery</title>
+  <title>Unife Tate Gallery - Home</title>
 </head>
+
 <body>
   <?php
-  include("../components/Navbar.php");
+  include("../components/navbar.php");
   ?>
-  <hr/>
 </body>
+
 </html>
