55aa3498eb2eb777dd54b6062de95d7855463429
Simone Acuti <acutisimone@gmail.com>
2024-06-29 15:44:00 +0200

    fix ddl

--- src/sql/ddl.sql
+++ src/sql/ddl.sql
@@ -4,7 +4,7 @@ USE unife_tate_gallery;
 
 CREATE TABLE artist (
   id INTEGER NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  name VARCHAR(300) NOT NULL,
   gender VARCHAR(10) NOT NULL,
   year_of_birth VARCHAR(10) NOT NULL,
   year_of_death VARCHAR(10) NOT NULL,
@@ -20,17 +20,17 @@ CREATE TABLE artwork (
   accession_number CHAR(7) NOT NULL,
   artist_role VARCHAR(100) NOT NULL,
   artist_id INTEGER NOT NULL,
-  title VARCHAR(300) NOT NULL,
+  title VARCHAR(400) NOT NULL,
   medium VARCHAR(300) NOT NULL,
-  credit_line VARCHAR(300) NOT NULL,
+  credit_line VARCHAR(900) NOT NULL,
   year VARCHAR(10) NOT NULL,
   acquisition_year VARCHAR(10) NOT NULL,
-  width VARCHAR(10) NOT NULL,
-  height VARCHAR(10) NOT NULL,
-  depth VARCHAR(10) NOT NULL,
+  width VARCHAR(20) NOT NULL,
+  height VARCHAR(20) NOT NULL,
+  depth VARCHAR(20) NOT NULL,
   units CHAR(2) NOT NULL,
   inscription VARCHAR(300) NOT NULL,
-  thumbnail_copyright VARCHAR(300) NOT NULL,
+  thumbnail_copyright VARCHAR(1000) NOT NULL,
   thumbnail_url VARCHAR(300) NOT NULL,
   url VARCHAR(300) NOT NULL,
 
@@ -43,6 +43,9 @@ CREATE TABLE artwork (
     | Warning | 1265 | Data truncated for column 'name' at row 3084 |
     | Warning | 1265 | Data truncated for column 'name' at row 3504 |
 */
+
+ -- MacOs path: /Applications/XAMPP/xamppfiles/htdocs/unife-tate-gallery/data/processed
+
 LOAD DATA INFILE 'X:/Sistema/Scuola/Informatica/GitHub/unife-tate-gallery/data/processed/artist_data.csv'
 INTO TABLE artist
 FIELDS TERMINATED BY ','
