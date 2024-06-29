cf850b123005ca78654a81b48dbb95de2df2614e
Simone Acuti <acutisimone@gmail.com>
2024-06-29 12:20:49 +0200

    feat add constants file

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php
+require("src/php/utils/constants.php");
 
 $request = $_SERVER['REQUEST_URI'];
 
 switch($request) {
-  case "/unife-tate-gallery/":
+  case "/$PROJECT_NAME/":
     require('src/php/views/home.php');
     break;
-  case "/unife-tate-gallery/artisti":
+  case "/$PROJECT_NAME/artisti":
     require('src/php/views/artisti.php');
     break;
-  case "/unife-tate-gallery/correlazioni":
+  case "/$PROJECT_NAME/correlazioni":
     require('src/php/views/correlazioni.php');
     break;
-  case "/unife-tate-gallery/opere":
+  case "/$PROJECT_NAME/opere":
     require('src/php/views/opere.php');
     break;
-  case "/unife-tate-gallery/statistiche":
+  case "/$PROJECT_NAME/statistiche":
     require('src/php/views/statistiche.php');
     break;
   default:
