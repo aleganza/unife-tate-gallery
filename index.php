da9a3c750f7db4db91a4d5a08f182337c55aeaaa
Simone Acuti <acutisimone@gmail.com>
2024-06-30 18:05:50 +0200

    feat: improove routing

--- index.php
+++ index.php
@@ -3,22 +3,25 @@ require("src/php/utils/constants.php");
 
 $request = parse_url($_SERVER['REQUEST_URI'])['path'];
 
-switch($request) {
-  case "/$PROJECT_NAME/":
-    require('src/php/views/home.php');
-    break;
-  case "/$PROJECT_NAME/artists":
-    require('src/php/views/artists.php');
-    break;
-  case "/$PROJECT_NAME/correlations":
-    require('src/php/views/correlations.php');
-    break;
-  case "/$PROJECT_NAME/artworks":
-    require('src/php/views/artworks.php');
-    break;
-  case "/$PROJECT_NAME/statistics":
-    require('src/php/views/statistics.php');
-    break;
+function abort() {
+  http_response_code(404);
+  require("src/php/views/404.php");
+  die();
+}
+
+$routes = [
+  "/$PROJECT_NAME/" => "src/php/views/home.php",
+  "/$PROJECT_NAME/artists" => "src/php/views/artists.php",
+  "/$PROJECT_NAME/correlations" => "src/php/views/correlations.php",
+  "/$PROJECT_NAME/artworks" => "src/php/views/artworks.php",
+  "/$PROJECT_NAME/statistics" => "src/php/views/statistics.php",
+];
+
+if(array_key_exists($request, $routes)) {
+  require($routes[$request]);
+}
+else {
+  abort();
 }
 
 ?>
