ac5f2f914600c6b8d22256c45c15bd6209cfc445
aleganza <ganzarolli.alessio@gmail.com>
2024-07-01 14:04:17 +0200

    Update head.php

--- src/php/components/head.php
+++ src/php/components/head.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="icon" type="image/x-icon" href="public/favicon.ico">
 <link rel="stylesheet" href="src/css/style.css">
+<link rel="icon" type="image/x-icon" href="public/logo.png">
 <title>Unife Tate Gallery</title>
